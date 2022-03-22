61606f125ae8dbe1a14e99bc3204279dd38de90d
Jochen Schalanda <jochen@schalanda.name>
2022-03-22 22:53:26 +0100

    Update and validate/clean up data in parallel

--- bin/update.bash
+++ bin/update.bash
@@ -6,8 +6,10 @@ set -Euo pipefail
 TEMP_DIR=$(mktemp -d)
 trap 'rm -rf ${TEMP_DIR}' EXIT
 
+SCRIPT_DIR=$(dirname "${0}")
+
 # shellcheck source=bin/functions.bash
-source "$(dirname "${0}")/functions.bash"
+source "${SCRIPT_DIR}/functions.bash"
 
 ROOT_DIR='./docs'
 ensure_directory "${ROOT_DIR}/checksums"
@@ -16,41 +18,39 @@ ensure_directory "${ROOT_DIR}/metadata"
 CHECKSUM_DIR=$(readlink -f "${ROOT_DIR}/checksums")
 METADATA_DIR=$(readlink -f "${ROOT_DIR}/metadata")
 
-for vendor in \
-	'adoptopenjdk' \
-	'temurin' \
-	'corretto' \
-	'semeru8' \
-	'semeru11' \
-	'semeru16' \
-	'semeru17' \
-	'graalvm-legacy' \
-	'graalvm' \
-	'zulu' \
-	'sapmachine' \
-	'liberica' \
-	'dragonwell8' \
-	'dragonwell11' \
-	'dragonwell17' \
-	'oracle' \
-	'openjdk' \
-	'openjdk-loom' \
-	'openjdk-panama' \
-	'openjdk-valhalla' \
-	'java-se-ri' \
-	'mandrel' \
-	'trava8' \
-	'trava11' \
-	'microsoft'
-do
-	cmd="$(dirname "${0}")/${vendor}.bash"
-	if [[ -x "${cmd}" ]]
-	then
-		"${cmd}" "${METADATA_DIR}/vendor" "${CHECKSUM_DIR}"
-	else
-		echo "Unable to update metadata for vendor ${vendor}"
-	fi
-done
+function cmd() {
+	echo "${SCRIPT_DIR}/${1}.bash"
+}
+
+vendors=(
+	"$(cmd 'adoptopenjdk')"
+	"$(cmd 'temurin')"
+	"$(cmd 'corretto')"
+	"$(cmd 'semeru8')"
+	"$(cmd 'semeru11')"
+	"$(cmd 'semeru16')"
+	"$(cmd 'semeru17')"
+	"$(cmd 'graalvm-legacy')"
+	"$(cmd 'graalvm')"
+	"$(cmd 'zulu')"
+	"$(cmd 'sapmachine')"
+	"$(cmd 'liberica')"
+	"$(cmd 'dragonwell8')"
+	"$(cmd 'dragonwell11')"
+	"$(cmd 'dragonwell17')"
+	"$(cmd 'oracle')"
+	"$(cmd 'openjdk')"
+	"$(cmd 'openjdk-loom')"
+	"$(cmd 'openjdk-panama')"
+	"$(cmd 'openjdk-valhalla')"
+	"$(cmd 'java-se-ri')"
+	"$(cmd 'mandrel')"
+	"$(cmd 'trava8')"
+	"$(cmd 'trava11')"
+	"$(cmd 'microsoft')"
+)
+
+printf '%s\n' "${vendors[@]}" | xargs -P 8 -t -I '%' -n 1 bash '%' "${METADATA_DIR}/vendor" "${CHECKSUM_DIR}"
 
 jq -s 'add' "${METADATA_DIR}"/vendor/*/all.json > "${METADATA_DIR}/all.json"
 aggregate_metadata "${METADATA_DIR}/all.json" "${METADATA_DIR}"
