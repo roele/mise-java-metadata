bbebad3b553d5b27a2c9b2271ff8144990cb02cd
Roland Schaer <roele33@hotmail.com>
2024-08-29 17:50:30 +0200

    Normalize Java SE RI versions (#22)

--- bin/java-se-ri.bash
+++ bin/java-se-ri.bash
@@ -66,7 +66,7 @@ do
 			"${VENDOR}" \
 			"${FILE}" \
 			"$(normalize_release_type "${VERSION}")" \
-			"${VERSION}" \
+			"$(normalize_version "${VERSION}")" \
 			"${VERSION}" \
 			'hotspot' \
 			"$(normalize_os "${OS}")" \
